23f732c8aad1d8f43b3ba53cd53d6b54b89b8413
Eric Beyerle <ebeyerle@uoregon.edu>
2020-11-09 17:24:27 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'LE4PD',
-    version = '0.3',
+    version = '0.1',
     author = 'Eric Beyerle',
     description = 'Python API for Protein Dynamics using the Langevin Formalism (LE4PD)',
     url = 'https://github.com/erb24/LE4PD',
