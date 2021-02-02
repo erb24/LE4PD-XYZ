2d21562aa635b33fe3cf560d722dd89becae3f84
Eric Beyerle <ebeyerle@uoregon.edu>
2021-02-02 12:37:18 -0800

    Update histogramming
    Accounts for different versions of physt

--- LE4PD/codes/LE4PD_XYZ.py
+++ LE4PD/codes/LE4PD_XYZ.py
@@ -387,7 +387,10 @@ def mode_mad(traj, protname, N, nfrs, Q, QINV, T, nmodes = 10, HA = False):
 			x=xim[a,:]*np.sin(theta[a,:])*np.cos(phi[a,:])
 			y=xim[a,:]*np.sin(theta[a,:])*np.sin(phi[a,:])
 			z=xim[a,:]*np.cos(theta[a,:])
-			h=physt.special.spherical_histogram(np.column_stack([x,y,z]),theta_bins=50,phi_bins=50,radial_bins=1)
+			if physt.__version__.split('.')[1] <= 3:
+				h=physt.special.spherical_histogram(np.column_stack([x,y,z]),theta_bins=50,phi_bins=50,radial_bins=1)
+			else:
+				h=physt.special_histograms.spherical(np.column_stack([x,y,z]),theta_bins=50,phi_bins=50,radial_bins=1)
 			his=(h.densities[0]/h.densities[0].sum()).T
 			fes = -kT*np.log(his)
 			#fes -= fes.min()
